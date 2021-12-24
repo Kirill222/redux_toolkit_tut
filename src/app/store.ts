1dc6feceba48fb162f326982a59e04b9d4aa697e
Zzz <zkmd2012@inbox.ru>
2021-12-24 10:48:50 +0100

    3 Created a reservations slice, exported reservations reducer and implemented in main store reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit"
+import reservationsReducer from '../features/reservationsSlice'
 
 export const store = configureStore({
     reducer: {
-
+        reservations: reservationsReducer
     }
 })
 
